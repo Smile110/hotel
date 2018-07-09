861bc801d09cc6ea8e40fe9c626597a4ab5802ec
yangruxia <rxyang@jointwisdom.cn>
2018-07-09 13:46:21 +0800

    添加footer样式

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ export default class Footer extends Component  {
     render(){
         return(
             <footer className="footer">
-                <div className="main clearfix">
+                <div className="main clearfix pd-30">
                     <div className="footer-container pull-left">
                         <dl>
                             <dt className="pl-8">
@@ -25,18 +25,23 @@ export default class Footer extends Component  {
                             <dt className="pl-8">
                                 加盟合作
                             </dt>
-                            <dd><a target="_blank" href="">火锅店</a></dd>
-                            <dd><a target="_blank" href="">酒店</a></dd>
+                            <dd><a target="_blank" href="https://www.baidu.com/">火锅店</a></dd>
+                            <dd><a target="_blank" href="https://github.com/">酒店</a></dd>
                         </dl>
                         <dl>
                             <dt className="pl-8">
                                 客服电话
                             </dt>
-                            <dd><a href="">12345678</a></dd>
+                            <dd><a>12345678</a></dd>
                         </dl>
                     </div>
                     <div className="pull-right">
                         <img src={code} alt=""/>
+                    </div>
+                </div>
+                <div className="copy-right">
+                    <div className="main">
+                        <p className="copy-right-info text-center">粤ICP备案14062877号  |  版权所有©2014 Homeinns Co.,Ltd All Rights Reserved.</p>
                     </div>
                 </div>
             </footer>
