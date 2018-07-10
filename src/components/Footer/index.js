fc9ba29dfddc2dfd3212b1bc7ddd98d4f747ae4c
zhouqihang1 <console_log@126.com>
2018-07-10 22:02:27 +0800

    footer优化

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,40 +5,45 @@ import React, {Component} from 'react';
 import FooterItem from './FooterItem';
 import code from '../../common/images/code.png';
 
+const footers = [
+    {
+        linkTitle: '关于我们',
+        linkItem: [
+            {title: '集团介绍', link: '/about#introduce', openWay: '_self'},
+            {title: '企业文化', link: '/about#culture', openWay: '_self'},
+            {title: '招贤纳士', link: '/about#job'},
+        ]
+    },
+    {
+        linkTitle: '加盟合作',
+        linkItem: [
+            {title: '酒店', link: 'https://github.com/', openWay: '_blank'},
+            {title: '住宿', link: 'https://www.baidu.com/', openWay: '_blank'},
+        ]
+    },
+    {
+        linkTitle: '客服电话',
+        linkItem: [
+            {title: '电话'},
+        ]
+    }
+];
+
 export default class Footer extends Component {
     constructor(props) {
         super(props)
     }
 
+    getFootersComponents() {
+        return footers.map((footer, index) => <FooterItem links={footer} key={index}/>);
+    }
+
     render() {
-        let aboutFooterLinks = {
-            linkTitle: '关于我们',
-            linkItem: [
-                {title: '集团介绍', link: '/about#introduce', openWay: '_self'},
-                {title: '企业文化', link: '/about#culture', openWay: '_self'},
-                {title: '招贤纳士', link: '/about#job'},
-            ]
-        };
-        let joinFooterLinks = {
-            linkTitle: '加盟合作',
-            linkItem: [
-                {title: '酒店', link: 'https://github.com/', openWay: '_blank'},
-                {title: '住宿', link: 'https://www.baidu.com/', openWay: '_blank'},
-            ]
-        };
-        let telFooterLinks = {
-            linkTitle: '客服电话',
-            linkItem: [
-                {title: '电话'},
-            ]
-        };
         return (
             <footer className="footer">
                 <div className="main clearfix pd-30">
                     <div className="footer-container pull-left">
-                        <FooterItem links={aboutFooterLinks}/>
-                        <FooterItem links={joinFooterLinks}/>
-                        <FooterItem links={telFooterLinks}/>
+                        { this.getFootersComponents() }
                     </div>
                     <div className="pull-right">
                         <img src={code} alt=""/>
